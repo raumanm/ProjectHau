74ba3fb07242c4e4353b4f912696365b6e1ceef8
Mikko Rauman <mikko.rauman@cs.tamk.fi>
2017-03-23 11:15:26 +0200

    Modify init function to support modular design.

--- backend/hau-db.js
+++ backend/hau-db.js
@@ -5,22 +5,20 @@
     let MongoDB, MongoClient, ObjectId, db, safeObjectId;
 
     MongoDB = require('mongodb');
+	
     MongoClient = MongoDB.MongoClient;
     ObjectId = MongoDB.ObjectId;
 
     const objectIdRe = /^[0-9a-fA-F]{24}$/;
     safeObjectId = s => objectIdRe.test(s) ? new ObjectId(s) : null;
 
-    function init() {
-        // Initialize connection once
-        MongoClient.connect("mongodb://localhost:27017/hauDb", (err, database) => {
-            if (err) throw err;
-            db = database;
-            exports.DB = db;
+    module.exports.init = function (callback) {
+        MongoClient.connect("mongodb://localhost:27017/hauDb", (err, client) => {
+            module.exports.db = client;
+			callback(err);
         });
     }
 
     exports.ObjectID = ObjectId;
     exports.safeObjectId = safeObjectId;
-    init();
 }());
