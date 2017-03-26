a9313ac874f51c11750e1f39544c88f770569edc
MikkoLuhtasaari <Mikko.Luhtasaari@gmail.com>
2017-03-26 16:59:03 +0300

    Create add-place.component.ts

---
+++ frontend/angulartest-app/src/app/add-place.component/add-place.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+import { PlaceService } from './add-place.service';
+import { Place } from '../classes/place';
+import { UtilsClass } from '../util/utilsclass';
+
+@Component({
+    moduleId: module.id,
+    selector: 'my-add-place',
+    templateUrl: 'add-place.component.html',
+    styleUrls: ['../stylesheets/formstyle.css']
+})
+export class AddPlaceComponent {
+  myForm: FormGroup;
+
+  constructor(fb: FormBuilder, private placeService: PlaceService) {
+    this.myForm = fb.group({
+      'name': [''],
+      'addressStreet': [''],
+      'addressCode': [''],
+      'addressCity': [''],
+      'visitationInterval': [''],
+      'pairAmount': [''],
+      'overseerId': ['']
+    });
+  }
+
+  onSubmit(value: string): void {
+    console.log(value);
+  }
+}
