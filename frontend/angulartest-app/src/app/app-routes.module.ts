93929357630a95cf8e26cc5f6f5790cf45e09b63
MikkoLuhtasaari <Mikko.Luhtasaari@gmail.com>
2017-03-27 17:53:03 +0300

    Update routes

--- frontend/angulartest-app/src/app/app-routes.module.ts
+++ frontend/angulartest-app/src/app/app-routes.module.ts
@@ -7,8 +7,9 @@ import { LoginPageComponent } from './login.component/login.component';
 import { PersonalInformationComponent } from './personal-information.component/personal-information.component';
 import { SearchComponent } from './search.component/search.component';
 import { AddDogComponent } from './add-dog.component/add-dog.component';
-import { AddPlaceComponent } from './add-place.component/add-place.component'
+import { AddPlaceComponent } from './add-place.component/add-place.component';
 import { AddUserComponent } from './add-user.component/add-user.component';
+import { VisitComponent } from './visit.component/visit.component';
 import { ReportComponent } from './report.component/report.component';
 import { MessageComponent } from './message.component/message.component';
 
@@ -21,6 +22,7 @@ export const routes: Routes = [
     { path: 'addDog', component: AddDogComponent },
     { path: 'addPlace', component: AddPlaceComponent },
     { path: 'addUser', component: AddUserComponent },
+    { path: 'addVisit', component: VisitComponent },
     { path: 'report', component: ReportComponent },
     { path: 'message', component: MessageComponent }
 ];
