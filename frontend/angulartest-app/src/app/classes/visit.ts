7dd6153f5f8db78465af1e1ca8844b2fcf3db414
MikkoLuhtasaari <Mikko.Luhtasaari@gmail.com>
2017-03-27 17:30:33 +0300

    Create visit class

---
+++ frontend/angulartest-app/src/app/classes/visit.ts
@@ -0,0 +1,15 @@
+/**
+ * Created by M1k1tus on 27-Mar-17.
+ */
+
+export class Visit {
+  constructor(
+    public visitTime: Date,
+    public placeId: string,
+    public assignedPairId: string,
+    public assignedPairStatus: string,
+    public details?: string
+  ) {}
+
+
+}
