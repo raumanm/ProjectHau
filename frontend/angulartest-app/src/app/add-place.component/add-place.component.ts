6ec415eb7eadce9a7f7a08ddd3b2e89ac1502798
MikkoLuhtasaari <Mikko.Luhtasaari@gmail.com>
2017-03-27 19:46:31 +0300

    Make better error alerts
    Error alerts now tell the user which inputfields to check. Additional information provided on pair amount and zip code.

--- frontend/angulartest-app/src/app/add-place.component/add-place.component.ts
+++ frontend/angulartest-app/src/app/add-place.component/add-place.component.ts
@@ -35,24 +35,39 @@ export class AddPlaceComponent {
   onSubmit(value: string): void {
     let everythingOk = true;
 
-    if(UtilsClass.validateShortOpenField(value["name"]) && UtilsClass.validateShortOpenField(value["addressStreet"]) &&
-    UtilsClass.validateShortOpenField(value["addressCity"]) && UtilsClass.validateShortOpenField(value["overseerId"])) {
-    } else {
+    if(!UtilsClass.validateShortOpenField(value["name"])) {
+      alert("Virhe! Tarkista syötteesi kohdasta nimi");
       everythingOk = false;
     }
 
-    if(UtilsClass.validateZipCode(value["addressCode"])) {
-    } else {
+    if(!UtilsClass.validateShortOpenField(value["addressStreet"])) {
+      alert("Virhe! Tarkista syötteesi kohdasta katuosoite");
       everythingOk = false;
     }
 
-    if(UtilsClass.validatePairAmount(value["pairAmount"])) {
-    } else {
+    if(!UtilsClass.validateShortOpenField(value["addressCity"])) {
+      alert("Virhe! Tarkista syötteesi kohdasta kaupunki");
       everythingOk = false;
     }
+
+    if(!UtilsClass.validateShortOpenField(value["overseerId"])) {
+      alert("Virhe! Tarkista syötteesi kohdasta kohdevastaava");
+      everythingOk = false;
+    }
+
+    if(!UtilsClass.validateZipCode(value["addressCode"])) {
+      alert("Virhe! Tarkista syötteesi kohdasta postinumero. Postinumerossa on viisi numeroa väliltä 0-9");
+      everythingOk = false;
+    }
+
+    if(!UtilsClass.validatePairAmount(value["pairAmount"])) {
+      alert("Virhe! Tarkista syötteesi kohdasta optimikoirakkomäärä. Optimikoirakkomäärän tulee olla väliltä 1-9");
+      everythingOk = false;
+    }
+
     if(value["details"] != "") {
-      if(UtilsClass.validateLongOpenField(value["details"])) {
-      } else {
+      if(!UtilsClass.validateLongOpenField(value["details"])) {
+        alert("Virhe! Tarkista syötteesi kohdasta lisätietoja");
         everythingOk = false;
       }
     }
@@ -61,8 +76,6 @@ export class AddPlaceComponent {
       //this.placeService.create(value);
       alert("Kohde lisätty onnistuneesti");
       console.log(value);
-    } else {
-      alert("Virhe! Tarkista syötteesi");
     }
   }
 }
