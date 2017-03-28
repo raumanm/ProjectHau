9da9b93a7d566bb36a341e338f6beb81e5081b21
Niko Mustonen <mustonen.niko@gmail.com>
2017-03-28 23:55:01 +0300

    Add response call for unauthorized action

--- backend/server.js
+++ backend/server.js
@@ -12,49 +12,52 @@
     express = require('express');
     bodyParser = require('body-parser');
 	hauMongo = require('./hau-db');
+    hauResponse = require('./hau-response');
 
+    //Authentication resource
     authentication = require('./authentication/authenticate-user');
-	hauDogs = require('./dogs/hau-dogs');
-	hauUsers = require('./users/hau-users');
-	hauPairs = require('./pairs/hau-pairs');
+	
+    //Rest resources
+    hauDogs =   require('./dogs/hau-dogs');
+	hauUsers =  require('./users/hau-users');
+	hauPairs =  require('./pairs/hau-pairs');
 	hauPlaces = require('./places/hau-places');
 	hauVisits = require('./visits/hau-visits');
 
+    //Set up application
     app = express();
     app.set('authenticationSecret', require('./authentication/authentication-config').secret);
-
     app.use( bodyParser.json() );
     app.set('json spaces', 2);
     app.enable('trust proxy');
     app.use(logger);
-    
-    var apiRoute = express.Router();
-    var jsonWebToken = require('jsonwebtoken');
-    
-    app.use(authentication);
+    app.use(authentication); //Set authentication route to app
 
+    //JsonWebToken used for user authentication
+    var jsonWebToken = require('jsonwebtoken');
+    var apiRoute = express.Router();
+
+    //Verify that user is logged in correctly
     app.use((req, res, next)=> {
+        
+        //Require authentication token from requests body or query
         var token = req.body.token || req.query.token || req.headers['x-access-token'];
         
         if(token) {
             jsonWebToken.verify(token, app.get('authenticationSecret'), (err, decoded)=> {
                 if(err) {
-                    return res.json({
-                        success: false,
-                        message: 'Authenticatoin failed'});
+                    res.json(hauResponse.createUnauthorizedResponse());
                 } else {
                     req.decoded = decoded;
                     next();
                 }
             });
         } else {
-            res.json({
-                success: false,
-                message: 'No token'
-            });
+            res.json(hauResponse.createUnauthorizedResponse());
         }
     });
 
+    //Set Rest resources under apiRoute
     apiRoute.use(hauDogs);
     apiRoute.use(hauUsers);
     apiRoute.use(hauPairs);
@@ -64,9 +67,6 @@
 
 	hauMongo.init(function (err) {
 		if (err) throw err;
-
 		app.listen(8080);
 	});
-
-
 }());
