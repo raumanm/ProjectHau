5900ff8c786c669c6d7ae3a5aa4dd915f18972c8
MikkoLuhtasaari <Mikko.Luhtasaari@gmail.com>
2017-04-02 17:10:36 +0300

    Add modify-user.component.ts

---
+++ frontend/angulartest-app/src/app/user.component/modify-user.component.ts
@@ -0,0 +1,33 @@
+/**
+ * Created by M1k1tus on 02-Apr-17.
+ */
+
+import {Component, OnInit} from '@angular/core';
+import {Router, ActivatedRoute, Params} from '@angular/router';
+
+import { UserService } from './user.service';
+import { User } from '../classes/user';
+import { UtilsClass } from '../util/utilsclass';
+import { AppComponent } from '../app.component';
+
+@Component({
+  //moduleId: module.id,
+  selector: 'my-modify-user',
+  templateUrl: './modify-user.component.html',
+  styleUrls: ['../stylesheets/formstyle.css']
+})
+export class ShowUserComponent implements OnInit {
+  user: User;
+
+  constructor(appComponent: AppComponent, private userService: UserService, private route: ActivatedRoute) {
+    appComponent.titleText = "Muokkaa käyttäjää";
+  }
+
+  ngOnInit(): void {
+    this.route.params
+      .switchMap((params: Params) => this.userService.getUser(params['id']))
+      .subscribe(user => this.user = user);
+  }
+
+
+}
