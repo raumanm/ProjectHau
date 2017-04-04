a2a4911d08769476826314e495c0fabad43e266e
Mikko Rauman <mikko.rauman@cs.tamk.fi>
2017-04-04 15:31:45 +0300

    Modify correct method call for put

--- backend/dogs/hau-dogs.js
+++ backend/dogs/hau-dogs.js
@@ -34,7 +34,7 @@
 	.get((req, res, next) => {
 		getById(req.params.id, (dog) => hauResponse.sendResponse(res, dog));
 	})
-    .update((req, res, next) => {
+    .put((req, res, next) => {
         if (req.get('Content-Type') === 'application/json') {
             updateById(req.params.id, req.body, (result) => hauResponse(res, result));
         }
