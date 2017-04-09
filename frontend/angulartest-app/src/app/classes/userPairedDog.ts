0fc212c097dd7faad9a6ff5119abd8b11f9c9af7
MikkoLuhtasaari <Mikko.Luhtasaari@gmail.com>
2017-04-09 15:00:00 +0300

    Create class

---
+++ frontend/angulartest-app/src/app/classes/userPairedDog.ts
@@ -0,0 +1,11 @@
+/**
+ * Created by M1k1tus on 09-Apr-17.
+ */
+
+export class userPairedDog {
+  constructor(
+    public nameFull: string,
+    public nameNickname: string,
+    public _id?: string
+  ) {}
+}
