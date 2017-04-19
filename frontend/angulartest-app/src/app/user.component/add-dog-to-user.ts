755222bf06d963f2efef8ff42f0e1d74960415c6
MikkoLuhtasaari <Mikko.Luhtasaari@gmail.com>
2017-04-19 20:20:37 +0300

    Rename class

---
+++ frontend/angulartest-app/src/app/user.component/add-dog-to-user.ts
@@ -0,0 +1,43 @@
+/**
+ * Created by M1k1tus on 19-Apr-17.
+ */
+
+import {Component, OnInit} from '@angular/core';
+import {Router, ActivatedRoute, Params} from '@angular/router';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+import { UserService } from './user.service';
+import { User } from '../classes/user';
+import { Dog } from '../classes/dog';
+import { AppComponent } from '../app.component';
+import {UtilsClass} from "../util/utilsclass";
+
+@Component({
+  moduleId: module.id,
+  selector: 'my-add-dog-to-user',
+  templateUrl: './add-dog-to-user.component.html',
+  styleUrls: ['../stylesheets/formstyle.css']
+})
+export class AddDogToUserComponent implements OnInit {
+  user: User;
+  myForm: FormGroup;
+
+  constructor(appComponent: AppComponent, private userService: UserService, private fb: FormBuilder, private route: ActivatedRoute, private router: Router) {
+    appComponent.titleText = "Lisää koira käyttäjälle";
+  }
+
+  ngOnInit(): void {
+    //Fetch user
+    this.route.params
+      .switchMap((params: Params) => this.userService.getUser(params['id']))
+      .subscribe(user => this.user = user);
+
+  }
+
+  onSubmit(value: string): void {
+    console.log("works");
+
+  }
+
+
+}
--- frontend/angulartest-app/src/app/user.component/add_dog_to_user.ts
+++
@@ -1,3 +0,0 @@
-/**
- * Created by M1k1tus on 19-Apr-17.
- */
