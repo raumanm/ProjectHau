741687b7a4eacf21d03606a858b7879fe4f0a146
MikkoLuhtasaari <Mikko.Luhtasaari@gmail.com>
2017-04-21 14:50:00 +0300

    Create class

---
+++ frontend/angulartest-app/src/app/classes/placeOverseer.ts
@@ -0,0 +1,11 @@
+/**
+ * Created by M1k1tus on 21-Apr-17.
+ */
+
+export class placeOverseer {
+  constructor(
+    public _id?: string,
+    public firstName?: string,
+    public lastName?: string
+  ) {}
+}
