8e2631fcc2fff54d7a82b551ba63e6b9facb11a1
MikkoLuhtasaari <Mikko.Luhtasaari@gmail.com>
2017-04-21 15:08:41 +0300

    Add onSubmit

--- frontend/angulartest-app/src/app/place.component/modify-place.component.ts
+++ frontend/angulartest-app/src/app/place.component/modify-place.component.ts
@@ -58,4 +58,51 @@ export class ModifyPlaceComponent implements OnInit {
         })
       );
   }
+
+  onSubmit(value: string): void {
+    let everythingOk = true;
+
+    if(!UtilsClass.validateShortOpenField(value["name"])) {
+      alert("Virhe! Tarkista syötteesi kohdasta nimi");
+      everythingOk = false;
+    }
+
+    if(!UtilsClass.validateShortOpenField(value["addressStreet"])) {
+      alert("Virhe! Tarkista syötteesi kohdasta katuosoite");
+      everythingOk = false;
+    }
+
+    if(!UtilsClass.validateShortOpenField(value["addressCity"])) {
+      alert("Virhe! Tarkista syötteesi kohdasta kaupunki");
+      everythingOk = false;
+    }
+
+    if(!UtilsClass.validateShortOpenField(value["overseerId"])) {
+      alert("Virhe! Tarkista syötteesi kohdasta kohdevastaava");
+      everythingOk = false;
+    }
+
+    if(!UtilsClass.validateZipCode(value["addressCode"])) {
+      alert("Virhe! Tarkista syötteesi kohdasta postinumero. Postinumerossa on viisi numeroa väliltä 0-9");
+      everythingOk = false;
+    }
+
+    if(!UtilsClass.validatePairAmount(value["pairAmount"])) {
+      alert("Virhe! Tarkista syötteesi kohdasta optimikoirakkomäärä. Optimikoirakkomäärän tulee olla väliltä 1-9");
+      everythingOk = false;
+    }
+
+    if(value["details"] != "") {
+      if(!UtilsClass.validateLongOpenField(value["details"])) {
+        alert("Virhe! Tarkista syötteesi kohdasta lisätietoja");
+        everythingOk = false;
+      }
+    }
+
+    if(everythingOk) {
+      this.placeService.modify(value);
+      alert("Kohde lisätty onnistuneesti");
+      console.log(value);
+    }
+  }
 }
