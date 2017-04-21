63077a872138c4de85f9c4fbce46faa1c2737f6b
MikkoLuhtasaari <Mikko.Luhtasaari@gmail.com>
2017-04-21 14:57:53 +0300

    Add base to class

--- frontend/angulartest-app/src/app/place.component/modify-place.component.ts
+++ frontend/angulartest-app/src/app/place.component/modify-place.component.ts
@@ -1,3 +1,32 @@
 /**
  * Created by M1k1tus on 21-Apr-17.
  */
+
+import {Component, OnInit} from '@angular/core';
+import {Router, ActivatedRoute, Params} from '@angular/router';
+
+import { UserService } from '../user.component/user.service';
+import { PlaceService } from './place.service';
+import { Place } from '../classes/place';
+import { UtilsClass } from '../util/utilsclass';
+import { AppComponent } from '../app.component';
+import { User } from "../classes/user";
+
+@Component({
+  selector: 'my-modify-place',
+  templateUrl: './modify-place.component.html',
+  styleUrls: ['../stylesheets/formstyle.css']
+})
+export class ModifyPlaceComponent implements OnInit {
+  place: Place;
+
+  constructor(appComponent: AppComponent, private placeService: PlaceService, private route: ActivatedRoute, private userService: UserService) {
+    appComponent.titleText = "Muokkaa kohdetta";
+  }
+
+  ngOnInit(): void {
+    this.route.params
+      .switchMap((params: Params) => this.placeService.getPlace(params['id']))
+      .subscribe(place => this.place = place);
+  }
+}
