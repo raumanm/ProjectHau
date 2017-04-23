f0b61e31aca63d8aac6509cf6929b846d8e4a6cb
Niko Mustonen <mustonen.niko@gmail.com>
2017-04-23 13:35:51 +0300

    Add token authentication asking from backend

--- frontend/angulartest-app/src/app/login.component/auth-quard.service.ts
+++ frontend/angulartest-app/src/app/login.component/auth-quard.service.ts
@@ -1,13 +1,38 @@
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Injectable } from '@angular/core';
 import { LoginService } from './login.service';
+import { Http, Headers, Response } from '@angular/http';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
 
-    constructor(private loginService: LoginService) {}
+    private headers: Headers = new Headers();
+
+    constructor(private loginService: LoginService, private http: Http) {
+        this.headers.append('Content-Type', 'application/json');
+    }
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
         return false;
+    }
+
+    authenticateToken(token) {
+        this.http.post('http://localhost:8080/checktoken', token, this.headers)
+            .map((res: Response) => {
+                
+                let token = res.json().data.token;
+                if (token) {
+                    let currentUser = {
+                        _id: res.json().data.userId,
+                        token : token,
+                        accessLevel: res.json().data.accessLevel
+                    };
+                    this.token = JSON.stringify(currentUser);
+                    localStorage.setItem('currentUser', token);
+                    return true;
+                } else {
+                    return false;
+                }
+            });
     }
 }
